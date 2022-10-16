45d4df61dc444f202e97c8dae7ab6785024d15f2
dnaylo-vasylyshyn <danylo.vasylyshyn@vitechteam.com>
2022-10-16 10:54:21 +0200

    Adding activation codes and evaluation-protocol relation and changing codes to uuid

--- src/sequelize/models/activation_code.model.js
+++ src/sequelize/models/activation_code.model.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-    sequelize.define('recovery_code', {
+    sequelize.define('activation_code', {
         code: {
             type: DataTypes.UUID,
             allowNull: false
