27dca4795b677fec9e00df58d9a9da01c1014081
dnaylo-vasylyshyn <danylo.vasylyshyn@vitechteam.com>
2022-10-18 15:18:48 +0200

    Cleaning code and adding email field verification

--- src/routes/upload_users.routes.js
+++ src/routes/upload_users.routes.js
@@ -6,8 +6,6 @@ const isEmail = require('../utils/validateEmail')
 
 const router = express.Router()
 router.use(fileUpload())
-const excelToJson = require('convert-excel-to-json');
-const isEmail = require('../utils/validateEmail')
 
 const User = sequelize.models.user
 const Evalueatee = sequelize.models.evaluatee
@@ -130,77 +128,6 @@ async function parse_excel(buffer) {
     catch(err){
         console.log(err)
     }
-    }
-}
-
-async function parse_csv(file){
-    var users = file.data.toString('utf8');
-    console.log(users);
-    const arr = users.split('\n');
-    for (var i = 0; i < arr.length; i++) {
-        let props = arr[i].split(',');
-        if (i == 0 && !isEmail(props[3])){
-            continue;
-        }
-        const user_identifier = await User.upsert(
-            {
-                first_name: props[1],
-                last_name: props[2],
-                academic_title: props[0],
-                email: props[3],
-                password: null,
-                account_status: 'inactive',
-                status_date: Date.now(),
-                user_type: props[4],
-            },
-            {
-              fields: ["first_name", "last_name", "academic_title", "user_type"],
-              conflictFields: ["email"]
-            }
-        );
-        await Evalueatee.upsert(
-            {userId: user_identifier[0].get("id"),
-            last_evaluated_date : props[5]
-            }, 
-            {
-                fields: ["last_evaluated_date"],
-                conflictFields: ["userId"]
-            }
-        );
-    }
-}
-
-async function parse_excel(buffer) {
-    const arr = Object.values(excelToJson({source: buffer}))[0];
-    for (var i = 0; i < arr.length; i++) {
-        if (i == 0 && !isEmail(arr[i].D)){
-            continue;
-        }
-        const user_identifier = await User.upsert(
-            {
-                first_name: arr[i].B,
-                last_name: arr[i].C,
-                academic_title: arr[i].A,
-                email: arr[i].D,
-                password: null,
-                account_status: 'inactive',
-                status_date: Date.now(),
-                user_type: arr[i].E,
-            },
-            {
-              fields: ["first_name", "last_name", "academic_title", "user_type"],
-              conflictFields: ["email"]
-            }
-        );
-        await Evalueatee.upsert(
-            {userId: user_identifier[0].get("id"),
-            last_evaluated_date : arr[i].F
-            }, 
-            {
-                fields: ["last_evaluated_date"],
-                conflictFields: ["userId"]
-            }
-        );
     }
 }
 
