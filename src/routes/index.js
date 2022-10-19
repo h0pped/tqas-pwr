c24dac2c74f1c470fbb1b0763ed9041bebc4d8d5
dnaylo-vasylyshyn <danylo.vasylyshyn@vitechteam.com>
2022-10-19 15:35:19 +0200

    CRUD endpoints for users

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
 const authRouter = require('./auth.routes')
+const userData = require('./user_data.router')
 
 const router = express.Router()
 
 router.use('/auth', authRouter)
+router.use('/user_data', userData)
 
 module.exports = router
