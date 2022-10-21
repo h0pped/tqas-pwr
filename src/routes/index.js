1bb394a4e1caa1988f6f1acb969e0ffb331fa88c
anton <anton.chernukha.ed@gmail.com>
2022-10-21 16:56:10 +0200

    Remove files that are not in main.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,12 @@
 const express = require('express')
 
 const authRouter = require('./auth.routes')
-const userData = require('./userData.router')
 const wzhzData = require('./wzhzData.router')
 const uploadUsersRouter = require('./uploadUsers.routes')
 
 const router = express.Router()
 
 router.use('/auth', authRouter)
-router.use('/userData', userData)
 router.use('/wzhzData', wzhzData)
 router.use('/uploadUsers', uploadUsersRouter)
 
