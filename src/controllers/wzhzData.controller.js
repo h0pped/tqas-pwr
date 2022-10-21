2f7f3264ea8933944b55a6c52205d2cd358c3648
anton <anton.chernukha.ed@gmail.com>
2022-10-21 17:21:24 +0200

    Add responses to config.

--- src/controllers/wzhzData.controller.js
+++ src/controllers/wzhzData.controller.js
@@ -1,4 +1,15 @@
 const sequelize = require('../sequelize')
+const StatusCodes = require('../config/statusCodes.config')
+
+const {
+    responseMessages: {
+        MEMBER_ADDED,
+        ALREADY_A_MEMBER,
+        USER_DOES_NOT_EXIST,
+        MEMBER_REMOVED,
+        MEMEBER_DOES_NOT_EXIST
+    },
+} = require('../config/index.config')
 
 const Wzhz = sequelize.models.wzhz
 const User = sequelize.models.user
@@ -29,15 +40,26 @@ module.exports.addMember = async (req, res) => {
             try {
                 const user = await Wzhz.create({ userId: userId })
                 console.log(userId)
-                return res.status(200).send({ message: `Member was added.`, user: user })
+                return res
+                    .status(StatusCodes[MEMBER_ADDED])
+                    .send({ 
+                        msg: MEMBER_ADDED, 
+                        user: user 
+                    })
             } catch (err) {
-                return res.status(500).json(err);
+                return res
+                    .status(StatusCodes[err.message] || 500)
+                    .send({ msg: err.message });
             }
         } else {
-            return res.status(400).send({ message: `This user is already a part of WZHZ group.` })
+            return res
+                .status(StatusCodes[ALREADY_A_MEMBER])
+                .send({ msg: ALREADY_A_MEMBER })
         }
     } else {
-        return res.status(400).send({ message: `User with this ID does not exist.` })
+        return res
+            .status(StatusCodes[USER_DOES_NOT_EXIST])
+            .send({ msg: USER_DOES_NOT_EXIST })
     }
 }
 
@@ -47,12 +69,18 @@ module.exports.removeMember = async (req, res) => {
     if (await memberExists(id)) {
         try {
             await Wzhz.destroy({ where: { id: id } })
-            return res.status(200).send({ message: `Member of WZHZ with id ${id} was deleted.` })
+            return res
+                .status(StatusCodes[MEMBER_REMOVED])
+                .send({ msg: MEMBER_REMOVED })
         } catch (err) {
-            return res.status(500).json(err);
+            return res
+                .status(StatusCodes[err.message] || 500)
+                .send({ msg: err.message });
         }
     } else {
-        return res.status(400).send({ message: `Member of WZHZ with id ${id} does not exist.` })
+        return res
+            .status(StatusCodes[MEMEBER_DOES_NOT_EXIST])
+            .send({ msg: MEMEBER_DOES_NOT_EXIST })
     }
 
 }
