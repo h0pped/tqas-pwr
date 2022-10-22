b58474d89c2b793b240c3af49096924e1d98083f
dnaylo-vasylyshyn <danylo.vasylyshyn@vitechteam.com>
2022-10-22 15:32:30 +0200

    Changing to checkAuthMiddleware

--- src/routes/userData.router.js
+++ src/routes/userData.router.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const {
-    authMiddleware,
+    checkAuthMiddleware,
 } = require('../middlewares/auth.middleware')
 
 const router = express.Router()
@@ -9,9 +9,9 @@ router.use(bodyParser.urlencoded())
 
 const {getUsers, updateUser, createUser, deleteUser} = require('../controllers/userData.controller')
 
-router.get('/getUsers', authMiddleware, getUsers)
-router.post('/updateUser', authMiddleware, updateUser)
-router.post('/createUser', authMiddleware, createUser)
-router.post('/deleteUser', authMiddleware, deleteUser)
+router.get('/getUsers', checkAuthMiddleware, getUsers)
+router.post('/updateUser', checkAuthMiddleware, updateUser)
+router.post('/createUser', checkAuthMiddleware, createUser)
+router.post('/deleteUser', checkAuthMiddleware, deleteUser)
 
 module.exports = router
