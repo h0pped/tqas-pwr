66fe74ee19858d2730f66d9214517765f680a839
dan4ikV <danylo.wasylyshyn@gmail.com>
2022-10-25 09:41:25 +0200

    Adding authentication check for the endpoint

--- src/routes/uploadUsers.routes.js
+++ src/routes/uploadUsers.routes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const fileUpload = require('express-fileupload')
+const {
+    authMiddleware,
+} = require('../middlewares/auth.middleware')
 
 const router = express.Router()
 router.use(fileUpload())
 
 const {appendUsers} = require('../controllers/uploadUsers.controller.js')
 
-router.post('/appendUsers', appendUsers)
+router.post('/appendUsers', authMiddleware, appendUsers)
 
 module.exports = router
