ef031dcb37994ceb9398925f948365ed01c52c33
dan4ikV <danylo.wasylyshyn@gmail.com>
2022-11-12 16:57:29 +0100

    Backend endpoint for setting evaluation teams

--- src/routes/evaluationsManagement.routes.js
+++ src/routes/evaluationsManagement.routes.js
@@ -5,11 +5,12 @@ const {
 
 const router = express.Router()
 
-const {createListOfClasses, setAssessmentSupervisor, createAssessment, getAssessments, getEvaluateesByAssessment, getAssessmentsBySupervisor } = require('../controllers/evaluationsManagement.controller.js')
+const {createListOfClasses, setAssessmentSupervisor, createAssessment, getAssessments, getEvaluateesByAssessment, getAssessmentsBySupervisor, createEvluationTeams} = require('../controllers/evaluationsManagement.controller.js')
 
 router.post('/createListOfClasses', authMiddleware, createListOfClasses)
 router.post('/setAssessmentSupervisor', authMiddleware, setAssessmentSupervisor)
 router.post('/createAssessment', authMiddleware, createAssessment)
+router.post('/createEvluationTeams', authMiddleware, createEvluationTeams)
 router.get('/getAssessments', authMiddleware, getAssessments)
 router.get('/getEvaluateesByAssessment', authMiddleware, getEvaluateesByAssessment)
 router.get('/getAssessmentsBySupervisor', authMiddleware, getAssessmentsBySupervisor)
