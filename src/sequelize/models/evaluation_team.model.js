cd02050cbf4f7752099aab9c17c9a65cb1ee99b0
dan4ikV <danylo.wasylyshyn@gmail.com>
2022-11-20 22:41:43 +0100

    Migrate database to store heads of evaluation teams

--- src/sequelize/models/evaluation_team.model.js
+++ src/sequelize/models/evaluation_team.model.js
@@ -1,5 +1,11 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-    sequelize.define('evaluation_team', {});
+    sequelize.define('evaluation_team', {
+        is_head_of_evaluation: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        }
+    });
 };
