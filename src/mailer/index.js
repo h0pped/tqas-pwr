a040f777f245d683c3992504516e588842b6a529
anton <anton.chernukha.ed@gmail.com>
2022-12-08 20:57:53 +0100

    rename

--- src/mailer/index.js
+++ src/mailer/index.js
@@ -1,10 +1,8 @@
 const nodemailer = require('nodemailer')
-//const { mailer } = require('../config/mailer.config')
 const transporter = nodemailer.createTransport({
     host: 'mail.pwr.wroc.pl',
     port: 25,
     tls: {
-        // do not fail on invalid certs
         rejectUnauthorized: false,
     },
 });
@@ -12,10 +10,9 @@ const transporter = nodemailer.createTransport({
 module.exports.sendMail = (email, title, content) => {
     const message = {
         to: email,
-        from: 'tqas@pwr.edu.pl',
+        from: 'Teaching Quality Assurance System <hospitacje@pwr.edu.pl>',
         subject: title,
         text: content,
-        //html: '1010101',
     }
     return new Promise((resolve, reject) => {
         transporter.sendMail(message, function (err, info) {
